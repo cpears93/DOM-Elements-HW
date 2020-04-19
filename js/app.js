c189111cadc1e8248f40891f98ff2e64728dae2e
Christopher Pearson <cpearson3140@gmail.com>
2020-04-19 20:02:50 +0100

    Populated app.js file

--- js/app.js
+++ js/app.js
@@ -1,2 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
+  const newItemform = document.querySelector('#new-item-form');
+  newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
+})
+
+const handleNewItemFormSubmit = function (event) {
+  event.preventDefault();
+
+  const favouriteSportsStarsItem = createfavouriteSportsStarsItem(event.target);
+  const favouriteSportsStars = document.querySelector('#favourite-sports-stars');
+  favouriteSportsStars.appendChild(readingListItem);
+
+  event.target.reset();
+}
+
+const createReadingListItem = function (form) {
+  const readingListItem = document.createElement('li');
+  favouriteSportsStarsItem.classList.add('favourite-sports-star-item');
+
+  const name = document.createElement('h2');
+  title.textContent = form.name.value;
+  favouriteSportsStarsItem.appendChild(name);
+
+  const age = document.createElement('h3');
+  author.textContent = form.age.value;
+  favouriteSportsStarsItem.appendChild(age);
+
+  const category = document.createElement('p');
+  category.textContent = form.category.value;
+  readingListItem.appendChild(sport);
+
+  return favouriteSportsStarsItem;
+}
+
+const handleDeleteAllClick = function (event) {
+  const favouriteSportsStars = document.querySelector('#favourite-sports-stars-list');
+  favouriteSportsStars.innerHTML = '';
+}
